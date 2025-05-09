a49f4cdd9c5239fc3751ae26fecb6843321de860
adsv31 <59443668+adsv31@users.noreply.github.com>
2025-05-09 15:37:53 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,29 @@
+document.querySelector('button').addEventListener('click', function(e) {
+  navigator.mediaDevices.getUserMedia({
+    audio: false,
+    video: {
+      facingMode: { exact: "environment" }
+    }
+  }).then(stream => {
+    var video = document.querySelector('video');
+    video.srcObject = stream;
+    video.onloadedmetadata = function() {
+      video.play();
+    };
+    document.documentElement.classList.add('streaming');
+  }).catch(err => {
+    console.log('error', err);
+  });
+});
+
+document.querySelector('aside').addEventListener('touchstart', e => {
+  e.preventDefault();
+});
+
+const options = Array.from(document.querySelectorAll('input[type=radio]'));
+
+options.forEach(option => {
+  option.addEventListener('click', e => {
+    document.documentElement.style.setProperty('--hue', e.currentTarget.getAttribute('data-hue'))
+  });
+});
