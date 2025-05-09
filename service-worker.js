92a312d096bdd06b6eea0ea7bc8f2a04d6de2863
adsv31 <59443668+adsv31@users.noreply.github.com>
2025-05-09 15:36:42 -0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,15 +1,18 @@
-self.addEventListener('install', function(e) {
+self.addEventListener('install', e => {
   e.waitUntil(
-    caches.open('code-noir-cache').then(function(cache) {
-      return cache.addAll(['./index.html', './manifest.json', './icon.png']);
+    caches.open('noir-cam').then(cache => {
+      return cache.addAll([
+        './index.html',
+        './style.css',
+        './script.js',
+        './manifest.json',
+        './icon.png'
+      ]);
     })
   );
 });
-
-self.addEventListener('fetch', function(e) {
+self.addEventListener('fetch', e => {
   e.respondWith(
-    caches.match(e.request).then(function(response) {
-      return response || fetch(e.request);
-    })
+    caches.match(e.request).then(response => response || fetch(e.request))
   );
 });
